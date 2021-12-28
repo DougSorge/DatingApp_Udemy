10a6adba48adeb8f8769ae651ad6aa7e62ffa79c
Doug Sorge <sorged91@gmail.com>
2021-12-28 12:04:52 -0500

    Add project files.

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,56 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    //declares the api endpoint..in this case we'll be at localhost:5001/api/users
+    //the users portion of the url comes from the first portion of the class name below. Since we have it named UsersController we get api/users. Notice the route statement below in the square brackets is setting api/controller as the path.
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET api/users/ID
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>>  GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+            
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,12 @@
+﻿//User Entity
+namespace API.Entities
+{
+    public class AppUser
+    {
+        //Entity framework uses naming conventions to recognize certain fields. This property needs to be named Id so that it will be recognized as a primary key. It will be set to auto increment by default.
+        public int ID { get; set; }
+
+        //This name also follows Entity conventions and needs the ? after the type in case the value in the database is null. It means the value is nullable.
+        public string UserName { get; set; }
+    }
+}
